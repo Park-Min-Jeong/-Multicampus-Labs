93695c4396da7d3bff6c66003a8c659baf095949
ParkMinjeong <minwjd1028@g.skku.edu>
2022-11-04 15:12:21 +0900

    221104_lab8 add answer

--- lab8.R
+++ lab8.R
@@ -25,3 +25,17 @@ daumnews <- data.frame(newscategory, newstitle)
 View(daumnews)
 write.csv(daumnews, "output/daumnews.csv")
 
+
+## answer
+### for & html_node 대신 html_nodes 사용
+### \"는 그대로 둬야 함 -- 웹에서 "를 표현하는 방식
+
+node_cate <- html_nodes(html, "ul.list_newsissue span.txt_category")
+newscategory <- html_text(node_cate)
+
+node_title <- html_nodes(html, "ul.list_newsissue a.link_txt")
+newstitle <- html_text(node_title, trim=T)
+
+daumnews <- data.frame(newscategory, newstitle)
+View(daumnews)
+write.csv(daumnews, "output/daumnews2.csv")
